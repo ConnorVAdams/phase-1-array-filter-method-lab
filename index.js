08d435080bf43ba233d2da8a2127330396abf03a
Connor Adams <connorvadams@gmail.com>
2023-09-17 15:33:36 -0600

    Submitting filter lab.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+const findMatching = (arr, driverName) => {
+    return arr.filter(element => {
+        return element.toLowerCase() === driverName.toLowerCase();
+    });
+};
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const fuzzyMatch = (arr, letters) => { //Pass in an array of drivers and a string of letters of any length.
+    const lengthOfStr = letters.length; //Determine the length of the string.
+    return arr.filter(element => { //Iterate through each element the array.
+        if (element.slice(0, lengthOfStr) === letters) { //Compare first two letters of active element to argument.
+            return element; //If it's a match, return that element (NOT FirstXLetters!)
+        };
+    });
+};
+
+const matchName = (arr, nameArg) => {
+    return arr.filter(element => {
+        return element.name === nameArg;
+    });
+};
